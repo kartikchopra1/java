519cc8638ef3729f8a073bf06535c97ffc3b852a
kartikchopra1 <42597800+kartikchopra1@users.noreply.github.com>
2018-08-28 10:02:40 +0530

    Add files via upload

---
+++ assignment2/Hello_world.java
@@ -0,0 +1,7 @@
+class Hello_world
+{
+	public static void main(String a[])
+	{
+		System.out.println("Hello, World!");
+	}
+}
---
+++ assignment2/Print_name.java
@@ -0,0 +1,7 @@
+class Print_name
+{
+	public static void main(String a[])
+	{
+		System.out.println("My name is Divyanshu Bali");
+	}
+}
